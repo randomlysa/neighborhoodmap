67c3d95f0a40b020e3e28629332c2d7deede4554
randomlysa <su@randomlysa.com>
2016-11-14 22:31:07 -0600

    load map markers from app.js

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var mapLocations = [
+    {
+        "title": "Orchard Lake (Sunset Photos)",
+        "coordinates": [44.695938, -93.307176]
+    },
+    {
+        "title": "Lebanon Hills Bike Trail (Mountain Biking)",
+        "coordinates": [44.78, -93.1897]
+    },
+    {
+        "title": "Spring Lake Park (Bike Trailhead)",
+        "coordinates": [44.768866, -92.928309]
+    }
+];
