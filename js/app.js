03fe2c98b75824157bbc2c5faf03a2fc388f6e82
randomlysa <su@randomlysa.com>
2016-11-15 21:52:38 -0600

    added infoWindow to each marker

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
 var mapLocations = [
     {
         "title": "Orchard Lake (Sunset Photos)",
-        "coordinates": [44.695938, -93.307176]
+        "coordinates": [44.695938, -93.307176],
+        "info": "One of the better places I've found to take sunset photos."
     },
     {
         "title": "Lebanon Hills Bike Trail (Mountain Biking)",
-        "coordinates": [44.78, -93.1897]
+        "coordinates": [44.78, -93.1897],
+        "info": "The beginner loop is enough for me..."
     },
     {
         "title": "Spring Lake Park (Bike Trailhead)",
-        "coordinates": [44.768866, -92.928309]
+        "coordinates": [44.768866, -92.928309],
+        "info": "One of the few trails I've found that I enjoy."
     },
     {
         "title": "Buck Hill (Skiing)",
-        "coordinates": [44.723454, -93.286449]
+        "coordinates": [44.723454, -93.286449],
+        "info": "I really should go here."
     },
     {
         "title": "Valley Lake Park (Quick Drone Flights)",
-        "coordinates": [44.716920, -93.209812]
+        "coordinates": [44.716920, -93.209812],
+        "info": "Emergency launch site for taking sunset photos."
     }
 ];
